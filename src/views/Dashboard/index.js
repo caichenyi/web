81d0c48c86872e9683f424914e7385109abc062e
chenyi.cai <chenyi.cai@aseit.cn>
2020-01-19 01:12:38 +0800

    配置外层路由

---
+++ src/views/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        仪表盘页面
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
