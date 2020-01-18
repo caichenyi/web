18498be768eb3b54bd9a1529426010b15288dff5
caichenyi <768683868@qq.com>
2020-01-18 15:12:04 +0800

    添加按需加载antd

--- config-overrides.js
+++ config-overrides.js
@@ -4,11 +4,17 @@
 
 const {
   override,
-  addLessLoader
+  addLessLoader,
+  fixBabelImports
 } = require('customize-cra');
 
 module.exports = override(
   addLessLoader({
     javascriptEnabled: true
-  })
+  }),
+  fixBabelImports('import', {
+    libraryName: "antd",
+    libraryDirectory: 'es',
+    style: true,
+  }),
 );
--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import {
+  Button
+} from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div>
         app
+        <Button>ANTD</Button>
       </div>
     );
   }
